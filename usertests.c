d21e096326ad9f493bce042d448e935fb2eafd03
RiddhiTharewal <tharewalrg19.comp@coep.ac.in>
2022-05-01 11:51:58 +0530

    Updated changes

--- usertests.c
+++ usertests.c
@@ -7,11 +7,164 @@
 #include "syscall.h"
 #include "traps.h"
 #include "memlayout.h"
+#include "shmem.h"
 
 char buf[8192];
 char name[3];
 char *echoargv[] = { "echo", "ALL", "TESTS", "PASSED", 0 };
 int stdout = 1;
+
+
+int key = 1234;
+int key0 = 2211;
+int key1 = 1111;
+int shmid,shmid0,shmid1;
+
+
+void test_read_and_write(){
+
+	char *string = "test_read_and_write";
+	int shmid = shmget(key,1024,02 | IPC_CREAT);
+	if(shmid == -1){
+		printf(1,"shmget fail");
+		exit();
+	}
+	char *str = (char*)shmat(shmid,(void*)0,0);
+	if((int)str < 0){
+		printf(1,"shmat failed");
+		exit();
+	}
+	for(int i = 0; string[i] != 0; i++) {
+		str[i] = string[i];
+	}
+	printf(1,"Data written in memory: %s\n",str);
+
+	int dt = shmdt(str);
+	if(dt<0){
+		printf(1,"shmdt fail");
+		exit();
+	}
+	str = (char*)shmat(shmid,(void*)0,0);
+	if((int)str<0){
+		printf(1,"second shmat failed");
+		exit();
+	}
+	printf(1,"Data read from memory: %s\n",str);
+
+	dt = shmdt(str);
+	if(dt<0){
+		printf(1,"Second shmdt failed");
+		exit();
+	}
+	int ct = shmctl(shmid,IPC_RMID,(void*)0);
+	if(ct<0){
+		printf(1,"shmctl failed\n");
+		exit();
+	}
+	printf(1,"Passed\n\n");
+}
+
+void shmget_test(){
+	printf(1,"\n\n");
+	shmid = shmget(key,1024,02|IPC_CREAT);
+	printf(1,"Checking if shared memory created: ");
+	if(shmid<0){
+		printf(1,"Failed\n");
+		exit();
+	}
+	printf(1,"Passed\n");
+	printf(1,"Creating Shared Memory of less than 0 : ");
+	shmid0 = shmget(key0,0,02|IPC_CREAT);
+	if(shmid0 == -1){
+		printf(1,"Passed\n");
+	}
+	else{
+		printf(1,"Failed\n");
+		exit();
+	}
+
+	printf(1,"Checking if shared memory created for size greater than available size: ");
+	shmid1 = shmget(key1,13200,02|IPC_PRIVATE);
+	if(shmid1==-1){
+		printf(1,"Passed\n");
+	}
+	else{
+		printf(1,"Failed\n");
+		exit();
+	}
+
+	printf(1,"Creating shared memory of already created memory using IPC_EXCL flag : ");
+	shmid = shmget(key,1024,02|IPC_CREAT|IPC_EXCL);
+	if(shmid == -1){
+		printf(1,"Passed\n");
+	}
+	else{
+		printf(1,"Failed\n");
+		exit();
+	}
+
+	printf(1,"Checking for key not equal to IPC_PRIVATE when shmflg is not IPC_CREAT: ");
+	shmid0 = shmget(key0,0,0);
+	if(shmid0 == -1){
+		printf(1,"Passed\n");
+	}
+	else{
+		printf(1,"Failed\n");
+		exit();
+	}
+	printf(1,"Tests for shmget: passed\n");
+}
+
+
+void shmat_test(){
+	printf(1,"\n\n");
+	shmid = shmget(key,1024,02|IPC_CREAT);
+	printf(1,"Basic shmat test:");
+	char *p = (char*)shmat(shmid,(void*)(0),0);
+	if((int)p!=-1){
+		printf(1,"Passed\n");
+	}
+	else{
+		printf(1,"Failed\n");
+		exit();
+	}
+	int d = shmdt((void*)p);
+	if(d==-1){
+		printf(1,"Detach failed\n");
+		exit();
+	}
+
+	printf(1,"Check if shmid out of bound");
+	p = (char*)shmat(45,(void*)0,0);
+	if((int)p==-1){
+		printf(1,"Passed\n");
+	}
+	else{
+		printf(1,"Failed\n");
+		exit();
+	}
+	printf(1,"Check if region with shmid is allocated:");
+	p = (char*)shmat(30,(void*)0,0);
+	if((int)p==-1){
+		printf(1,"Passed\n");
+	}
+	else{
+		printf(1,"Failed\n");
+		exit();
+	}
+
+	printf(1,"Check for shmaddr below MAXHEAP:");
+	p = (char*)shmat(shmid,(void*)(MAXHEAP - 100),0);
+	if((int)p==-1){
+		printf(1,"Passed\n");
+	}
+	else{
+		printf(1,"Failed\n");
+		exit();
+	}
+	printf(1,"Testing for shmat: passed\n\n");
+}
+
 
 // does chdir() call iput(p->cwd) in a transaction?
 void
@@ -1756,6 +1909,9 @@ main(int argc, char *argv[])
   }
   close(open("usertests.ran", O_CREATE));
 
+test_read_and_write();
+shmget_test();
+shmat_test();
   argptest();
   createdelete();
   linkunlink();
@@ -1798,6 +1954,7 @@ main(int argc, char *argv[])
   uio();
 
   exectest();
+
 
   exit();
 }
